618996551d6da86e11ec2255d9f3857e457fdcc8
SergiGBerlanga-DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2026-07-29 17:12:56 +0200

    Boss test scene, loop and shoot fixed, separated velocities and sprites and animation pivots edited

--- Assets/Scripts/Enemies/VultureBoss/VultureBossStates.cs
+++ Assets/Scripts/Enemies/VultureBoss/VultureBossStates.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 public class VultureBoss_AppearingState : IState
 {
     public VultureBoss v;
+    public float distanceToPatrol = 4;
     public void Enter(GameObject owner)
     {
         v = owner.GetComponent<VultureBoss>();
     }
     public void Update(GameObject owner)
     {
-        Vector2 targetPos = Controller.instance.transform.position + new Vector3(v.loopState.seekOffset.x, v.loopState.seekOffset.y);
+        Vector2 targetPos = Vector2.zero;//Controller.instance.transform.position + new Vector3(v.loopState.seekOffset.x, v.loopState.seekOffset.y);
         Vector2 dir = targetPos - new Vector2(v.transform.position.x, v.transform.position.y);
         v.rigidbody.linearVelocity = dir.normalized * v.speed;
-        if (Vector2.Distance(v.transform.position, targetPos) < v.targetDistanceTolerance)
+        if (Vector2.Distance(v.transform.position, targetPos) < distanceToPatrol)
             v.controller.ChangeState(v.loopState);
 
     }
@@ -30,6 +31,7 @@ public class VultureBoss_LoopState : IState
     public Vector2 seekOffset = new Vector2(0, 4);
     public float amplitude = 4f;
     public float height = 1.5f;
+    public float loopSpeed = 1;
     public float loopDuration = 5f;
     public float minTimeToShoot, maxTimeToShoot;
     float shotTimer;
@@ -47,7 +49,7 @@ public class VultureBoss_LoopState : IState
     }
     public void Update(GameObject owner)
     {
-        Vector2 center = Controller.instance.transform.position + new Vector3(seekOffset.x, seekOffset.y, 0);
+        Vector2 center = Vector2.zero;//Controller.instance.transform.position + new Vector3(seekOffset.x, seekOffset.y, 0);
         Vector2 offset = new Vector2(
             Mathf.Cos(phase) * amplitude,
             Mathf.Sin(phase * 2f) * height);
@@ -55,11 +57,13 @@ public class VultureBoss_LoopState : IState
         Vector2 tangent = new Vector2(
             -Mathf.Sin(phase) * amplitude,
              2f * Mathf.Cos(phase * 2f) * height);
-        phase += Time.deltaTime * v.speed / tangent.magnitude;
-
+        //phase += Time.deltaTime * v.speed / tangent.magnitude;
+        phase += Time.deltaTime * loopSpeed / tangent.magnitude;
+            
         shotTimer-= Time.deltaTime;
         if (shotTimer <= 0)
         {
+            shotTimer = Random.Range(minTimeToShoot, maxTimeToShoot);
             Vector2 dir = ((Vector2)Controller.instance.transform.position - (Vector2)owner.transform.position).normalized;
             GameObject bullet = PoolingManager.instance.GetInstanceOfClass("Bullet");
             bullet.transform.position = (Vector2)owner.transform.position + dir * 1f;
