59924d6d545f9eb78daf17b755a37cf531b95b9f
Garsudp <gerardoblasperez@gmail.com>
2026-09-13 17:04:00 +0200

    MainMenu DropDown Resolution functional

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,12 +1,16 @@
+using NUnit.Framework;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] TMP_Dropdown _resolutionDropdown;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       UIManager.Instance.SetDropdown(_resolutionDropdown, UIManager.Instance.GetResolutionOptions().optionDatas, UIManager.Instance.GetResolutionOptions().index, OnResolutionChanged);
     }
 
     // Update is called once per frame
@@ -23,5 +27,10 @@ public class MainMenu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    private void OnResolutionChanged(int index)
+    {
+        UIManager.Instance.ChangeResolution(UIManager.Instance.GetResolutions()[index]);
     }
 }
